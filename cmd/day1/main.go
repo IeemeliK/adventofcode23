0dbb43d80590ec3be2811c29e6b1172982e6f8b8
Eemeli Koikkalainen <eemeli.koikkalainen@outlook.com>
2024-01-06 00:05:53 +0200

    Solution for day one part two

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -27,15 +27,42 @@ func main() {
 }
 
 func parseNumbersFromString(txt string) string {
+    numberMap := map[string]string {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+
     var first, last string
+    firstout:
     for i := 0; i < len(txt); i++ {
+        for k, v := range numberMap {
+            if strings.HasPrefix(txt[i:], k) {
+                first = v
+                break firstout
+            }
+        }
+
         if _, err := strconv.Atoi(string(txt[i])); err == nil {
             first = string(txt[i])
             break
         }
     }
 
+    lastout:
     for i := len(txt) - 1; i >= 0; i-- {
+        for k, v := range numberMap {
+            if strings.HasPrefix(txt[i:], k) {
+                last = v
+                break lastout
+            }
+        }
         if _, err := strconv.Atoi(string(txt[i])); err == nil {
             last = string(txt[i])
             break
@@ -43,24 +70,3 @@ func parseNumbersFromString(txt string) string {
     }
     return first + last
 }
-
-// func stringToNumber(txt string) string {
-//     numberMap := map[string]string{
-//         "one": "1", 
-//         "two": "2", 
-//         "three": "3", 
-//         "four": "4", 
-//         "five": "5",
-//         "six": "6", 
-//         "seven": "7", 
-//         "eight": "8", 
-//         "nine": "9", 
-//     }
-//
-//     for key, value := range numberMap {
-//        if strings.Contains(txt, key) {
-//            strings.ReplaceAll(txt, key, value)
-//        } 
-//     }
-//     return txt
-// }
