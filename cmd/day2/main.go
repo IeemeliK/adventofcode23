b10bf63c3c455472c670522b9a621cba4ed3c402
Eemeli Koikkalainen <eemeli.koikkalainen@outlook.com>
2024-01-08 15:25:16 +0200

    Complete day2 part 1

--- cmd/day2/main.go
+++ cmd/day2/main.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,7 +19,57 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	var sum int
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		id, valid := test(scanner.Text())
+		if valid {
+			sum += id
+		}
 	}
+	fmt.Println(sum)
+}
+
+func test(game string) (int, bool) {
+	blueRegex := regexp.MustCompile(`(\d+)(?:\sblue)`)
+	greenRegex := regexp.MustCompile(`(\d+)(?:\sgreen)`)
+	redRegex := regexp.MustCompile(`(\d+)(?:\sred)`)
+
+	gameRegex := regexp.MustCompile(`Game\s(\d+)`)
+	id, _ := strconv.Atoi(gameRegex.FindStringSubmatch(game)[1])
+
+	cubeCounts := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+
+	sets := strings.Split(game, ";")
+
+	valid := false
+	for _, set := range sets {
+		var greenTotal, redTotal, blueTotal int
+		for _, match := range blueRegex.FindAllStringSubmatch(set, -1) {
+			if str, err := strconv.Atoi(match[1]); err == nil {
+				blueTotal += str
+			}
+		}
+
+		for _, match := range greenRegex.FindAllStringSubmatch(set, -1) {
+			if str, err := strconv.Atoi(match[1]); err == nil {
+				greenTotal += str
+			}
+		}
+
+		for _, match := range redRegex.FindAllStringSubmatch(set, -1) {
+			if str, err := strconv.Atoi(match[1]); err == nil {
+				redTotal += str
+			}
+		}
+		valid = (blueTotal <= cubeCounts["blue"] && greenTotal <= cubeCounts["green"] && redTotal <= cubeCounts["red"])
+		if !valid {
+			break
+		}
+	}
+
+	return id, valid
 }
